b290e67b1eb239abc5585fd2ba72450062af0f07
takayuki.kayawari <kayamelo@gmail.com>
2016-11-29 23:54:32 +0900

    test code

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @test_user = User.new(name: "てすと太郎", email:"sample@gmail.com")
+  end
+  
+  test "should be valid" do
+    assert @test_user.valid?
+  end 
 end
