2f6fc00acfaa12d12ae27a1a37617f8347e0001b
takayuki.kayawari <kayamelo@gmail.com>
2016-12-18 21:49:47 +0900

    テストデータにfactorygirlを利用

---
+++ spec/factories/todos.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+   factory :todo do
+    checked_flg false
+    title "サンプルタイトル"
+    memo "サンプルメモ"
+    duedate 2011-10-10
+   end
+end
--- spec/models/todos_spec.rb
+++ spec/models/todos_spec.rb
@@ -3,22 +3,18 @@ require 'rails_helper'
 describe Todo do
   describe 'Model' do
     it "is valid with a checked_flg, title, memo, duedate" do
-      todo = Todo.new(
-        checked_flg: false,
-        title: 'sample title',
-        memo: 'sample メモ',
-        duedate: 1991-10-20)
+      todo = FactoryGirl.build(:todo)
       expect(todo).to be_valid
     end
 
     it "is invalid without title" do
-      todo = Todo.new(checked_flg: false, title: nil)
+      todo = FactoryGirl.build(:todo, title:nil)
       todo.valid?
       expect(todo.errors[:title]).to include("can't be blank")
     end
     
     it "is valid with blank memo" do
-      todo = Todo.new(checked_flg: false, title:'sample', memo: nil)
+      todo = FactoryGirl.build(:todo, memo: nil)
       expect(todo).to be_valid
     end
   end
