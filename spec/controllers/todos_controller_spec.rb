03a6821b3260cd44c8b3f1d2050ae9a85cef11a9
takayuki.kayawari <kayamelo@gmail.com>
2016-12-24 22:23:47 +0900

    beforeフックでリファクタリング

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 describe TodosController do
+  before :each do
+    get :new
+  end
+  
   describe 'POST #new' do
     it "assigns :new Todo to @todo" do
-      get :new
       expect(assigns(:todo)).to be_a_new(Todo)
     end
   
     it "render :new template after create :new Todo" do
-      get :new
       expect(response).to render_template :new
     end
   end
