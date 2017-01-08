a44ba89c73a142a8a31921b304982ea950e9e647
takayuki.kayawari <kayamelo@gmail.com>
2017-01-09 02:49:33 +0900

    同じメールアドレスは禁止

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
 
   # TODO: 正直、ここの正規表現がわかっていない...
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -6,6 +6,12 @@ describe User do
     expect(user).to be_valid
   end
 
+  it "is invalid with a duplicate email" do
+    FactoryGirl.create(:user)
+    user = FactoryGirl.build(:user, email:"sample@gmail.com")
+    expect(user).to have(1).errors_on(:email)
+  end
+
   it "is invalid with illegal email" do
     user = FactoryGirl.build(:user, email:'hogehoge')
     expect(user).to have(1).errors_on(:email)
