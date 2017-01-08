8e69a36e6a4b18552a1e03f7c277a0cdb5b61bf5
takayuki.kayawari <kayamelo@gmail.com>
2017-01-09 03:39:31 +0900

    ユーザーの入力項目にパスワードを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   has_secure_password
+  validates :password, presence: true, length: {minimum: 8}
 end
