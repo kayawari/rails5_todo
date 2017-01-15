be267fbe1a112e5e9e0bf7132d129f9000aab87c
takayuki.kayawari <kayamelo@gmail.com>
2017-01-15 22:26:11 +0900

    ルーティングを修正
    ユーザー側からユーザー情報の編集、削除ができないようにする

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  root 'todos#index'
+
   resources :todos do
     member do
       post 'check'
     end
   end
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+
+  resources :users, only: [:index, :new, :create]
 
 end
