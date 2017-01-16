26e1c793ab60a3f56f33e078d619c2ee14c28028
takayuki.kayawari <kayamelo@gmail.com>
2017-01-17 00:51:39 +0900

    fix by rubocop & fix .rubocop.yml

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,12 +1,12 @@
 class Todo < ApplicationRecord
   validates :title, presence: true
-  validates :memo,  length: {maximum: 255}
+  validates :memo,  length: { maximum: 255 }
   validate :duedate_cannot_be_in_the_past
 
   # 登録日時より前の日時は登録できない
   def duedate_cannot_be_in_the_past
     if duedate < Date.today
-      errors.add(:duedate, "can not be in the past")
+      errors.add(:duedate, 'can not be in the past')
     end
   end
 end
