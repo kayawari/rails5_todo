216aa509de4bd1f5ee6758d67eef4e60824664af
takayuki.kayawari <kayamelo@gmail.com>
2017-04-17 23:52:21 +0900

    add login tool(imcompletion)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+
+  # 現在ログインしているユーザーを返す
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # ユーザーがログインしているかどうか確認
+  def logged_in?
+    !current_user.nil?
+  end
 end
