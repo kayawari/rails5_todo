2569939eecaa31f8d366401de171637be880aa4d
takayuki.kayawari <kayamelo@gmail.com>
2017-05-30 00:35:09 +0900

    コメントアウト削除

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,8 +3,6 @@ include ApplicationHelper
 class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
-  # GET /todos
-  # GET /todos.json
   def index
     unless logged_in?
       redirect_to login_path, alert: 'ログインしてください'
@@ -16,22 +14,16 @@ class TodosController < ApplicationController
     return @todos, @user
   end
 
-  # GET /todos/1
-  # GET /todos/1.json
   def show
   end
 
-  # GET /todos/new
   def new
     @todo = Todo.new
   end
 
-  # GET /todos/1/edit
   def edit
   end
 
-  # POST /todos
-  # POST /todos.json
   def create
     @todo = Todo.new(todo_params)
 
@@ -51,8 +43,6 @@ class TodosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /todos/1
-  # PATCH/PUT /todos/1.json
   def update
     respond_to do |format|
       if @todo.update(todo_params)
@@ -65,8 +55,6 @@ class TodosController < ApplicationController
     end
   end
 
-  # DELETE /todos/1
-  # DELETE /todos/1.json
   def destroy
     @todo.destroy
     respond_to do |format|
@@ -75,7 +63,6 @@ class TodosController < ApplicationController
     end
   end
 
-  # Todoの完了/未完了を切り替える
   def check
     @todo = Todo.find(params[:id])
 
@@ -87,12 +74,10 @@ class TodosController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_todo
     @todo = Todo.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def todo_params
     params.require(:todo).permit(:checked_flg, :title, :memo, :duedate)
   end
